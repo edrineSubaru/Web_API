392a3fdc0e13bce72d648c96628d7fdabe50b468
edrineSubaru <edrineviper@gmail.com>
2025-09-13 21:32:23 +0400

    another push working

--- GSI_Manage_BackEnd/GSI_Manage_BackEnd/Program.cs
+++ GSI_Manage_BackEnd/GSI_Manage_BackEnd/Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddScoped<GetDashboardStatsUseCase>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
